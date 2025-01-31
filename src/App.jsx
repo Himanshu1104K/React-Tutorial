d075019b11f2770dc9e479d1ad1f90697c0e531d
Himanshu Kumar <honey6k6@gmail.com>
2025-01-31 10:42:04 +0530

    UnderStanding Basic Concept of JSX and JavaScript.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const name = "Himanshu";
 
+  const name = <h1>Hello my name is Himanshu</h1>;
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <div>
+        <h1>Hello World.</h1>
+      </div> */}
+
+      {/* <div>
+        <h1>Hello my name is {name}</h1>
+      </div> */}
+
+      {/* {name}
+      {name} */}
     </>
-  )
+  );
 }
 
-export default App
+const GetName = () => {
+  return "Himanshu Kumar";
+};
+
+const GetNameComponent = () => {
+  return <h1>Himanshu Kumar</h1>;
+};
+
+export default App;
