3645dfe4bdb3ad1df0a6a092fd25a0bf71a9c68a
Himanshu Kumar <honey6k6@gmail.com>
2025-02-01 17:43:56 +0530

    Learning List and Mapping that list into Components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+// Lists in React
 
+// Example 1
+// function App() {
+//   const names = ["himanshu", "honey", "jon", "Singham", "Don"];
+
+//   return (
+//     <>
+//       <div className="App">
+//         {names.map((name, key) => {
+//           return <h1 key={key}>{name}</h1>;
+//         })}
+//       </div>
+//     </>
+//   );
+// }
+
+// Example 2
 function App() {
-  const [count, setCount] = useState(0)
+  const users = [
+    { name: "Himanshu", age: 21 },
+    { name: "Honey", age: 24 },
+    { name: "Jon", age: 19 },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        {users.map((user, key) => {
+          return (
+            <div key={key}>
+              <h1>{user.name}</h1>
+              <h1>{user.age}</h1>
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
