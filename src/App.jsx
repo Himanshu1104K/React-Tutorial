7f0adc84fe3ccb711864b3c72056348571b0be7c
Himanshu Kumar <honey6k6@gmail.com>
2025-02-01 20:27:34 +0530

    Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -18,25 +18,44 @@ import "./App.css";
 
 // Example 2
 function App() {
-  const users = [
-    { name: "Himanshu", age: 21 },
-    { name: "Honey", age: 24 },
-    { name: "Jon", age: 19 },
+  // const users = [
+  //   { name: "Himanshu", age: 21 },
+  //   { name: "Honey", age: 24 },
+  //   { name: "Jon", age: 19 },
+  // ];
+
+  // return (
+  //   <>
+  //     <div className="App">
+  //       {users.map((user, key) => {
+  //         return (
+  //           <div key={key}>
+  //             <h1>{user.name}</h1>
+  //             <h1>{user.age}</h1>
+  //           </div>
+  //         );
+  //       })}
+  //     </div>
+  //   </>
+  // );
+
+  const planets = [
+    { name: "Mars", isGasPlanet: false },
+    { name: "Earth", isGasPlanet: false },
+    { name: "Jupiter", isGasPlanet: true },
+    { name: "Venus", isGasPlanet: false },
+    { name: "Nepture", isGasPlanet: true },
+    { name: "Uranus", isGasPlanet: true },
   ];
 
   return (
-    <>
-      <div className="App">
-        {users.map((user, key) => {
-          return (
-            <div key={key}>
-              <h1>{user.name}</h1>
-              <h1>{user.age}</h1>
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <div className="App">
+      {planets.map((planet, key) => {
+        if (planet.isGasPlanet) {
+          return <h1 className="name">{planet.name}</h1>;
+        }
+      })}
+    </div>
   );
 }
 export default App;
