e8efa36b82e231af4cf61b8b25b2036cfa95a712
Himanshu Kumar <honey6k6@gmail.com>
2025-02-06 15:52:23 +0530

    Learning Basic fetching using Axios.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
+// Example 1
+// function App() {
+//   // fetch("https://catfact.ninja/fact")
+//   //   .then((res) => res.json())
+//   //   .then((data) => {
+//   //     console.log(data);
+//   //   });
+
+//   const [catFact, setCatFact] = useState("");
+
+//   const fetchCatFact = () => {
+//     Axios.get("https://catfact.ninja/fact").then((res) => {
+//       setCatFact(res.data.fact);
+//     });
+//   };
+
+//   useEffect(() => {
+//     fetchCatFact();
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="App">
+//         <button onClick={fetchCatFact} className="btn">
+//           Generate Cat Fact
+//         </button>
+//         <p>{catFact}</p>
+//       </div>
+//     </>
+//   );
+// }
+
+// Example 2
 function App() {
-  const [count, setCount] = useState(0)
+  const api = "https://api.agify.io/?name=";
+
+  const [name, setName] = useState("");
+  const [Age, setAge] = useState({});
+  const [inputDone, setInpDone] = useState(false);
+
+  const getName = (e) => {
+    setName(e.target.value);
+  };
+
+  const getAge = () => {
+    if (name !== "") {
+      Axios.get(`${api}${name}`).then((res) => {
+        setAge(res.data);
+      });
+    }
+    setInpDone(!inputDone);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {!inputDone && (
+        <input
+          type="text"
+          className="inp"
+          onChange={getName}
+          placeholder="Enter Name"
+        />
+      )}
+      <button onClick={getAge} className="btn">
+        Predict Age
+      </button>
+      {inputDone && (
+        <div>
+          <h1>Name : {Age?.name}</h1>
+          <h1>Predicted Age : {Age?.age}</h1>
+          <h1>Count : {Age?.count}</h1>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
