961534f19c508a60921f2f49b880f49effdf0a86
Himanshu Kumar <honey6k6@gmail.com>
2025-02-07 15:57:32 +0530

    Learning basic Functionality of React-Router-Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { Contact } from "./pages/Contact";
+import { AboutUs } from "./pages/AboutUs";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <Router>
+          <nav>
+            <div className="Links">
+              <Link to="/">Home</Link>
+              <Link to="/Contact">Contact Us</Link>
+              <Link to="/About_us">About Us</Link>
+            </div>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/About_us" element={<AboutUs />} />
+            <Route path="*" element={<h1>Error Wrong Route</h1>} />
+          </Routes>
+        </Router>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
