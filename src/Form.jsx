24075d7b7126d963e063f424f6453ee53fad96cd
Himanshu Kumar <honey6k6@gmail.com>
2025-02-13 21:59:02 +0530

    Adding errors and custom messages using yup

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,17 +3,21 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 export const Form = () => {
   const schema = yup.object().shape({
-    fullName: yup.string().required(),
-    email: yup.string().email().required(),
-    age: yup.number().positive().integer().min(18).required(),
-    password: yup.string().min(4).max(20).required(),
+    fullName: yup.string().required("Full Name is Required"),
+    email: yup.string().email().required("Email is Required"),
+    age: yup.number().positive().integer().min(18).required("Age is Required"),
+    password: yup.string().min(4).max(20).required("Password is Required"),
     confirmPassword: yup
       .string()
       .oneOf([yup.ref("password"), null], "Passwords must match")
-      .required(),
+      .required("Confirm Password is Required"),
   });
 
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
   });
 
@@ -30,18 +34,25 @@ export const Form = () => {
           placeholder="Full Name..."
           {...register("fullName")}
         />
+        {errors.fullName && <p>{errors.fullName.message}</p>}
         <input type="text" placeholder="Email..." {...register("email")} />
+        {errors.email && <p>{errors.email.message}</p>}
         <input type="number" placeholder="Age..." {...register("age")} />
+        {errors.age && <p>{errors.age.message}</p>}
         <input
           type="password"
           placeholder="Password..."
           {...register("password")}
         />
+        {errors.password && (
+          <p style={{ textWrap: "wrap" }}>{errors.password.message}</p>
+        )}
         <input
           type="password"
           placeholder="Confirm Password..."
           {...register("confirmPassword")}
         />
+        {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
         <input type="submit" />
       </form>
     </div>
