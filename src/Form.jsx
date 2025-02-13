727380682101e52afef1be66b359bb2087fdb6c1
Himanshu Kumar <honey6k6@gmail.com>
2025-02-13 21:43:08 +0530

    Learning Basic Form creation and Validation using useForm and yup

---
+++ src/Form.jsx
@@ -0,0 +1,49 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+export const Form = () => {
+  const schema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup.string().email().required(),
+    age: yup.number().positive().integer().min(18).required(),
+    password: yup.string().min(4).max(20).required(),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Passwords must match")
+      .required(),
+  });
+
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="form">
+      <h2>Enter Details</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Full Name..."
+          {...register("fullName")}
+        />
+        <input type="text" placeholder="Email..." {...register("email")} />
+        <input type="number" placeholder="Age..." {...register("age")} />
+        <input
+          type="password"
+          placeholder="Password..."
+          {...register("password")}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password..."
+          {...register("confirmPassword")}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
