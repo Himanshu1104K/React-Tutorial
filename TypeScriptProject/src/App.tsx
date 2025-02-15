2e976f48bd969764a68020b2593b2dd503c7c907
Himanshu Kumar <honey6k6@gmail.com>
2025-02-16 00:14:51 +0530

    Solving the Problems using TypeScript the same code is in the TypeScriptProject folder but with TS configurations.

---
+++ TypeScriptProject/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Person } from "./Person";
+
+function App() {
+  return (
+    <div className="App">
+      <Person
+        friends={["Jatin", "shivam", "Anushikha", "Shreya"]} // this will give us error on the console.
+        name="Himanshu"
+        email="himanshu@gmail.com"
+        age={20}
+        isMarried={false}
+      />
+    </div>
+  );
+}
+
+export default App;
